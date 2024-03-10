995d2cca475145682fcd70d5f16e8d8db5268f29
Mubashir Tanwar <119968206+MubashirTanwar@users.noreply.github.com>
2024-03-10 19:49:34 +0530

    Time: 7 ms (76.85%) | Memory: 9.7 MB (15.33%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -9,9 +9,9 @@ public:
         for (int i = 0; i < s.length(); i++) {
             HashMap[s[i]]++;
         }
-        for (int j = 0; j < t.length(); j++) {
-            if (HashMap.find(t[j]) != HashMap.end()) {
-                HashMap[t[j]] -= 1;
+        for (int i = 0; i < t.length(); i++) {
+            if (HashMap.find(t[i]) != HashMap.end()) {
+                HashMap[t[i]] -= 1;
             } else
                 return false;
         }
