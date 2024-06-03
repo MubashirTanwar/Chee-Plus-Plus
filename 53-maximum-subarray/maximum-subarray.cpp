a1368295e37fbfeb9037279c8f2f5adbaac38043
Mubashir Tanwar <119968206+MubashirTanwar@users.noreply.github.com>
2024-06-03 23:37:40 +0530

    Time: 562 ms (5.18%) | Memory: 70.2 MB (67.16%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -2,11 +2,15 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         double max = -numeric_limits<double>::infinity();
+        
         int end = 0;
         for(int i = 0; i <nums.size(); ++i){
-            end = end + nums[i];;
+            end = end + nums[i];
+            cout << end << endl;
             if(max < end) max = end;
             if (end < 0) end = 0;
+            cout << max;
+        
         }
         return max;
     }
