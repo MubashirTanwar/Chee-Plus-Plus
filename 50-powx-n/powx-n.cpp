3e29faeb0b34e9decb4bce6d0c400b77b7ddad50
Mubashir Tanwar <119968206+MubashirTanwar@users.noreply.github.com>
2024-06-10 00:45:41 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (9.98%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,25 +1,6 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        double res = 1.0;
-        while (n != 0)
-        {
-            if (n % 2 == 0)
-            {
-                x *= x;
-                n /= 2;
-            }
-            else if (n > 0)
-            {
-                res *= x;
-                --n;
-            }
-            else
-            {
-                res /= x;
-                ++n;
-            }
-        };
-        return res;
+        return pow(x,n);
     }
 };
