77dc2edc878144fc9b7c1743a0d42eaf31821cd1
Mubashir Tanwar <119968206+MubashirTanwar@users.noreply.github.com>
2026-01-12 20:48:58 +0530

    Time: 19 ms (76.93%) | Memory: 12.6 MB (73.96%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -8,19 +8,11 @@ class Solution(object):
         l,r,max_len = 0,0,0
         n = len(s)
         while r < n:
-            # while s[r] not in hashmap:
             if s[r] in hashmap:
-                print(s[r], "in hashmap", hashmap[s[r]] )
-                # l=hashmap[s[r]] +1
                 l = max(l, hashmap[s[r]] + 1)
-
-            
             hashmap[s[r]] = r
             r+=1
-            
             max_len = max(r-l,max_len)
-            print(max_len,l,r)
-        print(hashmap)
         return max_len
 
         
